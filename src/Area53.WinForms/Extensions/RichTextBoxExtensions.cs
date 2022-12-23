424dce6f8fccd29ee43651774ae69a78e1aa6003
Drake53 <49623303+Drake53@users.noreply.github.com>
2022-12-23 21:06:50 +0100

    Add some simple extension methods.

---
+++ src/Area53.WinForms/Extensions/RichTextBoxExtensions.cs
@@ -0,0 +1,40 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="RichTextBoxExtensions.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Area53.WinForms.Extensions
+{
+    public static class RichTextBoxExtensions
+    {
+        /// <summary>Sets the forecolor to be applied to the text when using <see cref="TextBoxBase.AppendText"/>.</summary>
+        public static void SetForeColor(this RichTextBox textBox, Color color)
+        {
+            textBox.SelectionStart = textBox.TextLength;
+            textBox.SelectionLength = 0;
+            textBox.SelectionColor = color;
+        }
+
+        /// <summary>Sets the backcolor to be applied to the text when using <see cref="TextBoxBase.AppendText"/>.</summary>
+        public static void SetBackColor(this RichTextBox textBox, Color backColor)
+        {
+            textBox.SelectionStart = textBox.TextLength;
+            textBox.SelectionLength = 0;
+            textBox.SelectionBackColor = backColor;
+        }
+
+        /// <summary>Sets the forecolor and backcolor to be applied to the text when using <see cref="TextBoxBase.AppendText"/>.</summary>
+        public static void SetColors(this RichTextBox textBox, Color color, Color backColor)
+        {
+            textBox.SelectionStart = textBox.TextLength;
+            textBox.SelectionLength = 0;
+            textBox.SelectionColor = color;
+            textBox.SelectionBackColor = backColor;
+        }
+    }
+}
---
+++ src/Area53.WinForms/Extensions/TextBoxBaseExtensions.cs
@@ -0,0 +1,26 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="TextBoxBaseExtensions.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.Windows.Forms;
+
+namespace Area53.WinForms.Extensions
+{
+    public static class TextBoxBaseExtensions
+    {
+        public static void AppendLine(this TextBoxBase textBox)
+        {
+            textBox.AppendText(Environment.NewLine);
+        }
+
+        public static void AppendLine(this TextBoxBase textBox, string text)
+        {
+            textBox.AppendText(text);
+            textBox.AppendLine();
+        }
+    }
+}
